046582e56555bde267d49bf088fa5373a3f26f6f
will <williancostajr@hotmail.com>
2022-12-20 08:27:30 -0300

    ccurso em video

--- tarefas/ex074.py
+++ tarefas/ex074.py
@@ -1,8 +1,12 @@
 # Exercício Python 074: Crie um programa que vai gerar cinco números aleatórios e colocar em uma tupla.
 # Depois disso, mostre a listagem de números gerados e também indique o menor e o maior valor que estão na tupla.
 
-import random
-num = int()
-c = 1
-num.__add__((random.randrange(6)))
-print(num)
+"""Tuplas são imutavéis, não da para acrescentara nem tirar"""
+from random import randint
+num = ((randint(0, 100)), (randint(0, 100)), (randint(0, 100)), (randint(0, 100)), (randint(0, 100)))
+print(f'Os números sorteados foram: ', end='')
+for c in num:
+    print(f'{c}', end=', ')
+print(f"""\nO menor valor foi o {min(num)}
+e o maior valor foi o {max(num)}""")
+
---
+++ tarefas/ex075.py
@@ -0,0 +1,12 @@
+# Exercício Python 075: Desenvolva um programa que leia quatro valores pelo teclado e guarde-os em uma tupla.
+# No final, mostre:
+#
+# A) Quantas vezes apareceu o valor 9.
+#
+# B) Em que posição foi digitado o primeiro valor 3.
+#
+# C) Quais foram os números pares.
+import random
+n1 = n2 = n3 = n4 = 0
+for c in range(1, 5):
+     = int(input())
