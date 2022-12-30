3c451293656f037e3a7cba1918100a12a346fcbf
willian <williancostajr@hotmail.com>
2022-12-30 01:21:17 -0300

    cvurso em video

--- tarefas/ex075.py
+++ tarefas/ex075.py
@@ -6,7 +6,20 @@
 # B) Em que posição foi digitado o primeiro valor 3.
 #
 # C) Quais foram os números pares.
-import random
-n1 = n2 = n3 = n4 = 0
-for c in range(1, 5):
-     = int(input())
+
+tupla = (int(input('Digite o 1º valor: ')),
+int(input('Digite o 2º valor: ')),
+int(input('Digite o 3º valor: ')),
+int(input('Digite o 4º valor: ')))
+if tupla.count(9):
+    print(f'O valor 9 apareceu {tupla.count(9)} vez(es).')
+else:
+    print('Não existe o valor 9 na tupla.')
+if tupla.count(3):
+    print(f'O primeiro valor 3 foi digitado no {tupla.index(3) + 1}º valor')
+else:
+    print(f'Não existe valor 3 na tupla.')
+print(f'Os números pares da tupla são:', end=' ')
+for n in tupla:
+    if n % 2 == 0:
+        print(n, end=' ')
---
+++ tarefas/ex076.py
@@ -0,0 +1,3 @@
+# Exercício Python 076:
+# Crie um programa que tenha uma tupla única com nomes de produtos e os seus respectivos preços, na sequência.
+# No final, mostre uma listagem de preços, organizando os dados em forma tabular.
