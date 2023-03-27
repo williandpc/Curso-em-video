b2fe678c59b09a94437e5be6e6bb7d9582c9f1f6
willian <williancostajr@hotmail.com>
2023-03-27 15:40:48 -0300

    Curso em video

---
+++ __init__.py
@@ -0,0 +1,15 @@
+import random
+
+parent = [-1, 0, 1, 2]
+parent2 = parent[:]
+print(parent)
+amp = int(len(parent) / 2)
+dicionario = {}
+for c in range(amp):
+    valor = random.choice(parent2)
+    dicionario[f"Dados {c + 1}"] = valor
+    del parent2[parent2.index(valor)]
+print(parent2)
+print(dicionario)
+for v, i in enumerate(parent):
+
